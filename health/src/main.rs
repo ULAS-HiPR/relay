9670967099e45ec851a2c416bf82d5df145cf4ea
Adam Byrne <adamrbyrne@gmail.com>
2023-12-26 03:50:12 +0000

    Refactored and added unit testing

--- health/src/main.rs
+++ health/src/main.rs
@@ -1,31 +1,16 @@
-use sys_info::{cpu_speed, loadavg, mem_info, proc_total};
-use config::{Config, File};
-use lapin::options::BasicPublishOptions;
-use lapin::{BasicProperties, Connection, ConnectionProperties};
-use serde::Deserialize;
 use tokio::time::{sleep, Duration};
 use std::time;
+use lapin::options::BasicPublishOptions;
+use lapin::{BasicProperties, Connection, ConnectionProperties};
+use config_loader::{ConfigLoader, ProductionConfigLoader};
 
-
-#[derive(Debug, Deserialize)]
-struct AmqpConfig {
-    host: String,
-    port: u16,
-    virtual_host: String,
-    username: String,
-    password: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct AppConfig {
-    amqp: AmqpConfig,
-}
+mod config_loader;
+mod system_info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut settings = Config::default();
-    settings.merge(File::with_name("../config")).unwrap();
-    let app_config: AppConfig = settings.try_into().unwrap();
+    let config_loader: Box<dyn ConfigLoader> = Box::new(ProductionConfigLoader);
+    let app_config = config_loader.load_config()?;
 
     let addr = format!(
         "amqp://{}:{}@{}:{}/{}",
@@ -37,44 +22,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     let conn = Connection::connect(&addr, ConnectionProperties::default()).await?;
-
     let channel = conn.create_channel().await?;
     let queue_name = "health";
-    channel
-        .queue_declare(queue_name, Default::default(), Default::default())
-        .await?;
+    channel.queue_declare(queue_name, Default::default(), Default::default()).await?;
 
     let duration = Duration::from_secs(1);
     loop {
-        let cpu_speed = cpu_speed().unwrap();
-        let loadavg = loadavg().unwrap();
-        let mem_info = mem_info().unwrap();
-        let proc_total = proc_total().unwrap();
         let current_time = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs();
+        let system_info = system_info::gather_system_info().await?;
 
+        let payload = system_info::format_payload(&system_info, current_time);
 
-        let payload = format!(
-            "Time: {}\nCPU Speed: {} MHz\nLoad Average: {:.2}, {:.2}, {:.2}\nMemory Info: Total: {} KB, Free: {} KB, Available: {} KB, Buffers: {} KB, Cached: {} KB\nTotal Processes: {}",
-            current_time,
-            cpu_speed,
-            loadavg.one, loadavg.five, loadavg.fifteen,
-            mem_info.total, mem_info.free, mem_info.avail, mem_info.buffers, mem_info.cached,
-            proc_total
-        );
-
-        channel
-            .basic_publish(
-                "",
-                queue_name,
-                BasicPublishOptions::default(),
-                payload.as_bytes(),
-                BasicProperties::default(),
-            )
-            .await?;
+        channel.basic_publish(
+            "",
+            queue_name,
+            BasicPublishOptions::default(),
+            payload.as_bytes(),
+            BasicProperties::default(),
+        ).await?;
 
         println!("[{}] Payload sent", current_time);
 
         sleep(duration).await;
     }
 }
-
