4bd3a4a31dc2964ed8910192be800248e01b0ce6
Adam Byrne <adamrbyrne@gmail.com>
2024-05-12 21:19:56 +0100

    lol didnt add code

--- camera/main.go
+++ camera/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"time"
 )
 
@@ -14,28 +13,27 @@ type Camera struct {
 	Height    int
 }
 
-func (c *Camera) Capture() {
+func (c *Camera) Capture() error {
 	timestamp := time.Now().Format(time.RFC3339)
 	filepath := fmt.Sprintf("%s/%s.jpg", c.OutputDir, timestamp)
 	cmd := exec.Command("libcamera-jpeg", "-o", filepath, "--width", fmt.Sprintf("%d", c.Width), "--height", fmt.Sprintf("%d", c.Height), "--signal")
-
 	err := cmd.Run()
 	if err != nil {
-		log.Println(err)
+		return err
 	}
+	return nil
 }
 
 func (c *Camera) Init() error {
 	if err := os.MkdirAll(c.OutputDir, 0755); err != nil {
 		return err
 	}
-
 	return nil
 }
 
 func main() {
 	camera := &Camera{
-		OutputDir: "/home/agrisat/results/camera",
+		OutputDir: "/home/agrisat/relay/camera/results",
 		Width:     640,
 		Height:    480,
 	}
@@ -48,9 +46,16 @@ func main() {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	for {
-		<-ticker.C
-		camera.Capture()
-		fmt.Println("Picture taken")
-	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for {
+			<-ticker.C
+			camera.Capture()
+			fmt.Println("Picture taken")
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
