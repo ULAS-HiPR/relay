58582f72223afcac67b9374c651de14cb2ee41df
Adam Byrne <adamrbyrne@gmail.com>
2024-06-26 00:22:41 +0100

    bash port (UNTESTED)

---
+++ camera/README.md
@@ -0,0 +1,29 @@
+# camera
+
+Camera is a service running the libcamera library to capture images from a HQ Raspberry Pi FishEye camera and save them to disk. These images are watched by the agrinet service to generate synthetic NIR images from them.
+
+## Installation
+
+```bash
+sudo apt install libcamera libcamera-apps
+
+chmod +x ./deploy.sh 
+./deploy.sh # Deploy the service (auto start on deploy)
+```
+
+*Note: deploy the service on code change as it will not auto-reload.*
+
+## Usage
+
+```bash
+sat_camera # Start the service
+killall sat_camera # Stop the service
+```
+
+## Debugging
+
+Use `gdb` to attach to the running process and debug it - can be done live or after the fact.
+
+```bash
+gdb -p $(pgrep -f sat_camera) # Attach to the running process (named)
+```
--- camera/deploy.sh
+++ camera/deploy.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-PROGRAM_NAME="ASCamera"
+
+# Name of the service
+EXECUTABLE="sat_camera"
+echo "Deploying $EXECUTABLE service..."
+
 SOURCE_DIR="/home/agrisat/relay/camera"
-SERVICE_DIR="/etc/systemd/system"
-GO_BINARY="/usr/local/bin/camera"
-GO_PROGRAM="$SOURCE_DIR/main.go"
+OUTPUT_DIR="/home/agrisat/relay/results/camera"
 
-go build -o $GO_BINARY $GO_PROGRAM
+chmod +x $SOURCE_DIR/"camera.sh"
+echo "Service executable permissions set"
 
-sudo tee "$SERVICE_DIR/$PROGRAM_NAME.service" > /dev/null << EOF
-[Unit]
-Description=AgriSat Camera Service
-After=network.target
-[Service]
-Type=simple
-Restart=always
-ExecStart=$GO_BINARY
-[Install]
-WantedBy=multi-user.target
-EOF
+# wipe the contents of the output directory
+rm -rf $OUTPUT_DIR/*
+echo "Output directory cleaned"
 
-sudo systemctl daemon-reload
-sudo systemctl enable "$PROGRAM_NAME.service"
-sudo systemctl start "$PROGRAM_NAME.service"
-sudo systemctl status "$PROGRAM_NAME.service"
+# Copy the script as a bin file
+cp $SOURCE_DIR/$EXECUTABLE /usr/local/bin/$EXECUTABLE
+
+echo "Starting $EXECUTABLE service..."
+$EXECUTABLE
